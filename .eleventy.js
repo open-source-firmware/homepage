0fe0fa89ca2bc6f264f32294519cb5f3ac0488de
denitze <denise.brachwitz@gmail.com>
2021-04-28 10:50:28 +0200

    changeds js-active

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
 module.exports = (config) => {
-  // Set directories to pass through to the dist folder
-  config.addPassthroughCopy("./src/images/");
   config.addPassthroughCopy("./src/fonts/");
+  // Transforms
+  const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+
+  // Create a helpful production flag
+  const isProduction = process.env.NODE_ENV === "production";
+
+  // Only minify HTML if we are in production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
+
   // config.addPassthroughCopy("./src/scripts/");
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
--- src/index.md
+++ src/index.md
@@ -1,5 +1,6 @@
 ---
 title: "Open Source Firmware Foundation"
+metaDesc: "The Open-Source Firmware Foundation is a non-profit organisation founded in 2021 with the goal to support various free and open-source firmware projects."
 layout: "layouts/home.html"
 bigLogo: true
 ---
